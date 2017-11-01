16060b7e11657d53c42b7318724a7bdc70392096
Nazrin Binyaminova <nazrinbinyaminova@gmail.com>
2017-11-01 18:00:48 +0400

    Home page slider finally

--- js/script.js
+++ js/script.js
@@ -22,33 +22,19 @@ $(document).ready(function(){
 	})
 
 
-	// $(".dropDownMenuFeatures").hide();
-	// $(".menuDropFeatures").hover(function(){
-	// 	$(".dropDownMenuFeatures").fadeIn();
-	// }, function(){
-	// 	$(".dropDownMenuFeatures").fadeOut();
-	// });
+	$(".dropDownMenuFeatures").hide();
+	$(".menuDropFeatures").hover(function(){
+		$(".dropDownMenuFeatures").fadeIn();
+	}, function(){
+		$(".dropDownMenuFeatures").fadeOut();
+	});
 
-	// $(".dropDownMenuEnter").hide();
-	// $(".menuDropEnter").hover(function(){
-	// 	$(".dropDownMenuEnter").fadeIn();
-	// }, function(){
-	// 	$(".dropDownMenuEnter").fadeOut();
-	// });
-	
-	// $(".menuDropFeatures").hover(function(){
-	// 	$(".dropDownMenuFeatures").animate(
-	// 		{display: "block"},1000,
-	// 		function(){
-	// 			$(".dropDownMenuFeatures").animate({opacity: "1"}, 1000)
-	// 		});
-	// }, function(){
-	// 	$(".dropDownMenuFeatures").animate(
-	// 		{opacity: "0"},
-	// 		function(){
-	// 			$(".dropDownMenuFeatures").css("display", "none")
-	// 		})
-	// });
+	$(".dropDownMenuEnter").hide();
+	$(".menuDropEnter").hover(function(){
+		$(".dropDownMenuEnter").fadeIn();
+	}, function(){
+		$(".dropDownMenuEnter").fadeOut();
+	});
 
 
 	$(".smallNavBar .listBtn i").click(function(){
@@ -115,5 +101,91 @@ $(document).ready(function(){
 		$(this).parent("li").addClass("active");
 	})
 
-	$()
+	
+	count=0;
+	$(window).resize(function(){
+		var slideWidth=$("#homeSliderSection .sliderSection").width();
+		var slideCount=$("#homeSliderSection .slideImage").length;
+		$("#homeSliderSection ul li").width(slideWidth/slideCount-3);
+	})
+
+	$("#homeSliderSection .slideBtnRight").click(function(){
+		var slideCount=$("#homeSliderSection .slideImage").length;
+		count++;
+		if(count==slideCount){
+			count=0;
+		}
+		$("#homeSliderSection .slideImage").hide();
+		$("#homeSliderSection .slideImage").eq(count).removeClass("displayNone");
+		$("#homeSliderSection .slideImage").eq(count).fadeIn();
+		$("#homeSliderSection ul li").addClass("activeBackGray");
+		$("#homeSliderSection ul li").removeClass("activeBackRed");
+		$("#homeSliderSection ul li").eq(count).addClass("activeBackRed");
+	})
+
+	$("#homeSliderSection .slideBtnLeft").click(function(){
+		var slideCount=$("#homeSliderSection .slideImage").length;
+		count--;
+		if(count<0){
+			count=slideCount-1;
+		}
+		$("#homeSliderSection .slideImage").hide();
+		$("#homeSliderSection .slideImage").eq(count).removeClass("displayNone");
+		$("#homeSliderSection .slideImage").eq(count).fadeIn();
+		$("#homeSliderSection ul li").addClass("activeBackGray");
+		$("#homeSliderSection ul li").removeClass("activeBackRed");
+		$("#homeSliderSection ul li").eq(count).addClass("activeBackRed");
+	})
+
+	count2=0;
+	$(".rightSlider .slideBtnLeft").click(function(){
+		var slideCount=$(".rightSlider .slideImage").length;
+		count2--;
+		if(count2<0){
+			count2=slideCount-1;
+		}
+		$(".rightSlider .slideImage").hide();
+		$(".rightSlider .slideImage").eq(count2).removeClass("displayNone");
+		$(".rightSlider .slideImage").eq(count2).fadeIn();
+	})
+
+	$(".rightSlider .slideBtnRight").click(function(){
+		var slideCount=$(".rightSlider .slideImage").length;
+		count2++;
+		if(count2==slideCount){
+			count2=0;
+		}
+		$(".rightSlider .slideImage").hide();
+		$(".rightSlider .slideImage").eq(count2).removeClass("displayNone");
+		$(".rightSlider .slideImage").eq(count2).fadeIn();
+	})
+
+	count3=0;
+	$(".slowSlider .homeSlider").width($(".slowSlider .slowSlideItem").width()*5)
+	$(".slowSlider .slideBtnLeft").click(function(){
+		var slideWidth=$(".slowSlider .slowSlideItem").width();
+		var uzun=slideWidth*2
+		if(count3=0){
+			$(".slowSlider .homeSlider").css({
+				position: relative,
+				left: -uzun
+			});
+			count3=1;
+		}else{
+			$(".slowSlider .homeSlider").css("left","(slideWidth*2)px");
+			count3=0;
+		}
+	})
+
+	$(".slowSlider .slideBtnRight").click(function(){
+		var slideCount=$(".slowSlider .slowSlideItem").length;
+		count3++;
+		if(count3==slideCount){
+			count3=0;
+		}
+		$(".slowSlider .slowSlideItem").hide();
+		$(".slowSlider .slowSlideItem").eq(count3).removeClass("displayNone");
+		$(".slowSlider .slowSlideItem").eq(count3).fadeIn();
+	})
 })
+
