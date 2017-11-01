84c007154db503eedd7593a89fc15ee67c6d21fc
Nazrin Binyaminova <nazrinbinyaminova@gmail.com>
2017-11-01 04:01:43 +0400

    update menu

--- js/script.js
+++ js/script.js
@@ -60,7 +60,8 @@ $(document).ready(function(){
 		if ($(this).parent("li").attr("class")!="activeSmall") {
 			$(this).parent("li").addClass("activeSmall");
 		}else{
-			$(this).parent("li").removeClass("activeSmall");		}
+			$(this).parent("li").removeClass("activeSmall");		
+		}
 	});
 
 	$(".smallNavBarItem .fa-angle-down").click(function(){
@@ -76,4 +77,43 @@ $(document).ready(function(){
 		$(".smallNavBar .left input").show();
 		$(".smallNavBar .left a").hide();
 	});
+	
+	$(".enterOrchard ul li a").click(function(event){
+		event.preventDefault();
+		$(".entertainmentItem").hide();
+		var index=$(".enterOrchard ul li a").index(this);
+		$(".entertainmentItem").eq(index).fadeIn(800);
+		$(".enterOrchard ul li a").removeClass("activeList");
+		$(this).addClass("activeList");
+		$(".enterOrchard ul li a").removeClass("activeGreen");
+		$(this).addClass("activeGreen");
+		$(".enterOrchard ul li a").css("color", "gray");
+		$(this).css("color", "green");	
+	})	
+
+	$(".worldNews ul li a").click(function(event){
+		event.preventDefault();
+		$(".worldNewsItem").hide();
+		var index=$(".worldNews ul li a").index(this);
+		$(".worldNewsItem").eq(index).fadeIn(800);
+		$(".worldNews ul li a").removeClass("activeList");
+		$(this).addClass("activeList");
+		$(".worldNews ul li a").removeClass("activeOrange");
+		$(this).addClass("activeOrange");
+		$(".worldNews ul li a").css("color", "gray");
+		$(this).css("color", "darkorange");	
+	})
+
+	$(".rightMenu ul li a").click(function(event){
+		event.preventDefault();
+		$(".newsRightTop").hide();
+		var index=$(".rightMenu ul li a").index(this);
+		$(".newsRightTop").eq(index).fadeIn(800);
+		$(".rightMenu ul li").removeClass("activeRedBorder");
+		$(this).parent("li").addClass("activeRedBorder");
+		$(".rightMenu ul li").removeClass("active");
+		$(this).parent("li").addClass("active");
+	})
+
+	$()
 })
